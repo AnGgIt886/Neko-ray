2dust/v2rayNG

Standardized file naming

V2rayNG/app/src/main/java/com/v2ray/ang/handler/PluginServiceManager.kt
@@ -1,4 +1,4 @@
-package com.v2ray.ang.util
+package com.v2ray.ang.handler
 
 import android.content.Context
 import android.os.SystemClock
@@ -7,11 +7,12 @@ import com.v2ray.ang.AppConfig
 import com.v2ray.ang.dto.EConfigType
 import com.v2ray.ang.dto.ProfileItem
 import com.v2ray.ang.fmt.Hysteria2Fmt
-import com.v2ray.ang.handler.SpeedtestManager
 import com.v2ray.ang.service.ProcessService
+import com.v2ray.ang.util.JsonUtil
+import com.v2ray.ang.util.Utils
 import java.io.File
 
-object PluginUtil {
+object PluginServiceManager {
     private const val HYSTERIA2 = "libhysteria2.so"
 
     private val procService: ProcessService by lazy {

V2rayNG/app/src/main/java/com/v2ray/ang/receiver/TaskerReceiver.kt
@@ -5,7 +5,7 @@ import android.content.Context
 import android.content.Intent
 import android.text.TextUtils
 import com.v2ray.ang.AppConfig
-import com.v2ray.ang.service.V2RayServiceManager
+import com.v2ray.ang.handler.V2RayServiceManager
 
 class TaskerReceiver : BroadcastReceiver() {
 

V2rayNG/app/src/main/java/com/v2ray/ang/receiver/WidgetProvider.kt
@@ -10,7 +10,7 @@ import android.os.Build
 import android.widget.RemoteViews
 import com.v2ray.ang.AppConfig
 import com.v2ray.ang.R
-import com.v2ray.ang.service.V2RayServiceManager
+import com.v2ray.ang.handler.V2RayServiceManager
 
 class WidgetProvider : AppWidgetProvider() {
     /**

V2rayNG/app/src/main/java/com/v2ray/ang/service/QSTileService.kt
@@ -13,6 +13,7 @@ import androidx.annotation.RequiresApi
 import androidx.core.content.ContextCompat
 import com.v2ray.ang.AppConfig
 import com.v2ray.ang.R
+import com.v2ray.ang.handler.V2RayServiceManager
 import com.v2ray.ang.util.MessageUtil
 import com.v2ray.ang.util.Utils
 import java.lang.ref.SoftReference

V2rayNG/app/src/main/java/com/v2ray/ang/service/V2RayVpnService.kt
@@ -20,7 +20,9 @@ import com.v2ray.ang.AppConfig.LOOPBACK
 import com.v2ray.ang.AppConfig.VPN_MTU
 import com.v2ray.ang.BuildConfig
 import com.v2ray.ang.handler.MmkvManager
+import com.v2ray.ang.handler.NotificationManager
 import com.v2ray.ang.handler.SettingsManager
+import com.v2ray.ang.handler.V2RayServiceManager
 import com.v2ray.ang.util.MyContextWrapper
 import com.v2ray.ang.util.Utils
 import java.lang.ref.SoftReference
@@ -28,7 +30,7 @@ import java.lang.ref.SoftReference
 class V2RayVpnService : VpnService(), ServiceControl {
     private lateinit var mInterface: ParcelFileDescriptor
     private var isRunning = false
-    private var tun2SocksManager: Tun2SocksManager? = null
+    private var tun2SocksService: Tun2SocksService? = null
 
     /**destroy
      * Unfortunately registerDefaultNetworkCallback is going to return our VPN interface: https://android.googlesource.com/platform/frameworks/base/+/dda156ab0c5d66ad82bdcf76cda07cbc0a9c8a2e
@@ -85,7 +87,7 @@ class V2RayVpnService : VpnService(), ServiceControl {
 
     override fun onDestroy() {
         super.onDestroy()
-        NotificationService.cancelNotification()
+        NotificationManager.cancelNotification()
     }
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
@@ -271,7 +273,7 @@ class V2RayVpnService : VpnService(), ServiceControl {
      * Starts the tun2socks process with the appropriate parameters.
      */
     private fun runTun2socks() {
-        tun2SocksManager = Tun2SocksManager(
+        tun2SocksService = Tun2SocksService(
             context = applicationContext,
             vpnInterface = mInterface,
             isRunningProvider = { isRunning },
@@ -299,8 +301,8 @@ class V2RayVpnService : VpnService(), ServiceControl {
             }
         }
 
-        tun2SocksManager?.stopTun2Socks()
-        tun2SocksManager = null
+        tun2SocksService?.stopTun2Socks()
+        tun2SocksService = null
 
         V2RayServiceManager.stopCoreLoop()
 
@@ -320,3 +322,4 @@ class V2RayVpnService : VpnService(), ServiceControl {
         }
     }
 }
+

V2rayNG/app/src/main/java/com/v2ray/ang/ui/MainActivity.kt
@@ -38,7 +38,7 @@ import com.v2ray.ang.handler.AngConfigManager
 import com.v2ray.ang.handler.MigrateManager
 import com.v2ray.ang.handler.MmkvManager
 import com.v2ray.ang.helper.SimpleItemTouchHelperCallback
-import com.v2ray.ang.service.V2RayServiceManager
+import com.v2ray.ang.handler.V2RayServiceManager
 import com.v2ray.ang.util.Utils
 import com.v2ray.ang.viewmodel.MainViewModel
 import kotlinx.coroutines.Dispatchers

V2rayNG/app/src/main/java/com/v2ray/ang/ui/MainRecyclerAdapter.kt
@@ -26,7 +26,7 @@ import com.v2ray.ang.handler.AngConfigManager
 import com.v2ray.ang.handler.MmkvManager
 import com.v2ray.ang.helper.ItemTouchHelperAdapter
 import com.v2ray.ang.helper.ItemTouchHelperViewHolder
-import com.v2ray.ang.service.V2RayServiceManager
+import com.v2ray.ang.handler.V2RayServiceManager
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch

V2rayNG/app/src/main/java/com/v2ray/ang/ui/ScSwitchActivity.kt
@@ -2,7 +2,7 @@ package com.v2ray.ang.ui
 
 import android.os.Bundle
 import com.v2ray.ang.R
-import com.v2ray.ang.service.V2RayServiceManager
+import com.v2ray.ang.handler.V2RayServiceManager
 
 class ScSwitchActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {

AnGgIt886/Neko-ray

V2rayNG/app/src/main/java/com/neko/v2ray/handler/PluginServiceManager.kt
@@ -1,4 +1,4 @@
-package com.neko.v2ray.util
+package com.neko.v2ray.handler
 
 import android.content.Context
 import android.os.SystemClock
@@ -7,11 +7,12 @@ import com.neko.v2ray.AppConfig
 import com.neko.v2ray.dto.EConfigType
 import com.neko.v2ray.dto.ProfileItem
 import com.neko.v2ray.fmt.Hysteria2Fmt
-import com.neko.v2ray.handler.SpeedtestManager
 import com.neko.v2ray.service.ProcessService
+import com.neko.v2ray.util.JsonUtil
+import com.neko.v2ray.util.Utils
 import java.io.File
 
-object PluginUtil {
+object PluginServiceManager {
     private const val HYSTERIA2 = "libhysteria2.so"
 
     private val procService: ProcessService by lazy {

V2rayNG/app/src/main/java/com/neko/v2ray/receiver/TaskerReceiver.kt
@@ -5,7 +5,7 @@ import android.content.Context
 import android.content.Intent
 import android.text.TextUtils
 import com.neko.v2ray.AppConfig
-import com.neko.v2ray.service.V2RayServiceManager
+import com.neko.v2ray.handler.V2RayServiceManager
 
 class TaskerReceiver : BroadcastReceiver() {
 

V2rayNG/app/src/main/java/com/neko/v2ray/receiver/WidgetProvider.kt
@@ -10,7 +10,7 @@ import android.os.Build
 import android.widget.RemoteViews
 import com.neko.v2ray.AppConfig
 import com.neko.v2ray.R
-import com.neko.v2ray.service.V2RayServiceManager
+import com.neko.v2ray.handler.V2RayServiceManager
 
 class WidgetProvider : AppWidgetProvider() {
     /**

V2rayNG/app/src/main/java/com/neko/v2ray/service/QSTileService.kt
@@ -13,6 +13,7 @@ import androidx.annotation.RequiresApi
 import androidx.core.content.ContextCompat
 import com.neko.v2ray.AppConfig
 import com.neko.v2ray.R
+import com.neko.v2ray.handler.V2RayServiceManager
 import com.neko.v2ray.util.MessageUtil
 import com.neko.v2ray.util.Utils
 import java.lang.ref.SoftReference

V2rayNG/app/src/main/java/com/neko/v2ray/service/V2RayVpnService.kt
@@ -20,7 +20,9 @@ import com.neko.v2ray.AppConfig.LOOPBACK
 import com.neko.v2ray.AppConfig.VPN_MTU
 import com.neko.v2ray.BuildConfig
 import com.neko.v2ray.handler.MmkvManager
+import com.neko.v2ray.handler.NotificationManager
 import com.neko.v2ray.handler.SettingsManager
+import com.neko.v2ray.handler.V2RayServiceManager
 import com.neko.v2ray.util.MyContextWrapper
 import com.neko.v2ray.util.Utils
 import java.lang.ref.SoftReference
@@ -28,7 +30,7 @@ import java.lang.ref.SoftReference
 class V2RayVpnService : VpnService(), ServiceControl {
     private lateinit var mInterface: ParcelFileDescriptor
     private var isRunning = false
-    private var tun2SocksManager: Tun2SocksManager? = null
+    private var tun2SocksService: Tun2SocksService? = null
 
     /**destroy
      * Unfortunately registerDefaultNetworkCallback is going to return our VPN interface: https://android.googlesource.com/platform/frameworks/base/+/dda156ab0c5d66ad82bdcf76cda07cbc0a9c8a2e
@@ -85,7 +87,7 @@ class V2RayVpnService : VpnService(), ServiceControl {
 
     override fun onDestroy() {
         super.onDestroy()
-        NotificationService.cancelNotification()
+        NotificationManager.cancelNotification()
     }
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
@@ -271,7 +273,7 @@ class V2RayVpnService : VpnService(), ServiceControl {
      * Starts the tun2socks process with the appropriate parameters.
      */
     private fun runTun2socks() {
-        tun2SocksManager = Tun2SocksManager(
+        tun2SocksService = Tun2SocksService(
             context = applicationContext,
             vpnInterface = mInterface,
             isRunningProvider = { isRunning },
@@ -299,8 +301,8 @@ class V2RayVpnService : VpnService(), ServiceControl {
             }
         }
 
-        tun2SocksManager?.stopTun2Socks()
-        tun2SocksManager = null
+        tun2SocksService?.stopTun2Socks()
+        tun2SocksService = null
 
         V2RayServiceManager.stopCoreLoop()
 
@@ -320,3 +322,4 @@ class V2RayVpnService : VpnService(), ServiceControl {
         }
     }
 }
+

V2rayNG/app/src/main/java/com/neko/v2ray/ui/MainActivity.kt
@@ -41,7 +41,7 @@ import com.neko.v2ray.handler.AngConfigManager
 import com.neko.v2ray.handler.MigrateManager
 import com.neko.v2ray.handler.MmkvManager
 import com.neko.v2ray.helper.SimpleItemTouchHelperCallback
-import com.neko.v2ray.service.V2RayServiceManager
+import com.neko.v2ray.handler.V2RayServiceManager
 import com.neko.v2ray.util.Utils
 import com.neko.v2ray.viewmodel.MainViewModel
 import kotlinx.coroutines.Dispatchers

V2rayNG/app/src/main/java/com/neko/v2ray/ui/MainRecyclerAdapter.kt
@@ -27,7 +27,7 @@ import com.neko.v2ray.handler.AngConfigManager
 import com.neko.v2ray.handler.MmkvManager
 import com.neko.v2ray.helper.ItemTouchHelperAdapter
 import com.neko.v2ray.helper.ItemTouchHelperViewHolder
-import com.neko.v2ray.service.V2RayServiceManager
+import com.neko.v2ray.handler.V2RayServiceManager
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch

V2rayNG/app/src/main/java/com/neko/v2ray/ui/ScSwitchActivity.kt
@@ -2,7 +2,7 @@ package com.neko.v2ray.ui
 
 import android.os.Bundle
 import com.neko.v2ray.R
-import com.neko.v2ray.service.V2RayServiceManager
+import com.neko.v2ray.handler.V2RayServiceManager
 
 class ScSwitchActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {