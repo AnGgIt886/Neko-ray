2dust/v2rayNG

Add Enable New TUN Feature option
When enabled, TUN will use hev-socks5-tunnel; otherwise, it will use badvpn-tun2socks

V2rayNG/app/src/main/java/com/v2ray/ang/service/TProxyService.kt
@@ -1,0 +1,96 @@
+package com.v2ray.ang.service
+
+import android.content.Context
+import android.os.ParcelFileDescriptor
+import android.util.Log
+import com.v2ray.ang.AppConfig
+import com.v2ray.ang.AppConfig.VPN_MTU
+import com.v2ray.ang.handler.MmkvManager
+import com.v2ray.ang.handler.SettingsManager
+import java.io.File
+
+/**
+ * Manages the tun2socks process that handles VPN traffic
+ */
+class TProxyService(
+    private val context: Context,
+    private val vpnInterface: ParcelFileDescriptor,
+    private val isRunningProvider: () -> Boolean,
+    private val restartCallback: () -> Unit
+) : Tun2SocksControl {
+    companion object {
+        @JvmStatic
+        @Suppress("FunctionName")
+        private external fun TProxyStartService(configPath: String, fd: Int)
+        @JvmStatic
+        @Suppress("FunctionName")
+        private external fun TProxyStopService()
+        @JvmStatic
+        @Suppress("FunctionName")
+        private external fun TProxyGetStats(): LongArray?
+
+        init {
+            System.loadLibrary("hev-socks5-tunnel")
+        }
+    }
+
+    /**
+     * Starts the tun2socks process with the appropriate parameters.
+     */
+    override fun startTun2Socks() {
+        Log.i(AppConfig.TAG, "Starting HevSocks5Tunnel via JNI")
+
+        val configContent = buildConfig()
+        val configFile = File(context.filesDir, "hev-socks5-tunnel.yaml").apply {
+            writeText(configContent)
+        }
+        Log.i(AppConfig.TAG, "Config file created: ${configFile.absolutePath}")
+        Log.d(AppConfig.TAG, "Config content:\n$configContent")
+
+        try {
+            Log.i(AppConfig.TAG, "TProxyStartService...")
+            TProxyStartService(configFile.absolutePath, vpnInterface.fd)
+        } catch (e: Exception) {
+            Log.e(AppConfig.TAG, "HevSocks5Tunnel exception: ${e.message}")
+        }
+    }
+
+    private fun buildConfig(): String {
+        val socksPort = SettingsManager.getSocksPort()
+        val vpnConfig = SettingsManager.getCurrentVpnInterfaceAddressConfig()
+        return buildString {
+            appendLine("tunnel:")
+            appendLine("  mtu: $VPN_MTU")
+            appendLine("  ipv4: ${vpnConfig.ipv4Client}")
+
+            if (MmkvManager.decodeSettingsBool(AppConfig.PREF_PREFER_IPV6) == true) {
+                appendLine("  ipv6: '${vpnConfig.ipv6Client}'")
+            }
+
+            appendLine("socks5:")
+            appendLine("  port: ${socksPort}")
+            appendLine("  address: ${AppConfig.LOOPBACK}")
+            appendLine("  udp: 'udp'")
+
+            MmkvManager.decodeSettingsString(AppConfig.PREF_LOGLEVEL)?.let { logPref ->
+                if (logPref != "none") {
+                    val logLevel = if (logPref == "warning") "warn" else logPref
+                    appendLine("misc:")
+                    appendLine("  log-level: $logLevel")
+                }
+            }
+        }
+    }
+
+    /**
+     * Stops the tun2socks process
+     */
+    override fun stopTun2Socks() {
+        try {
+            Log.i(AppConfig.TAG, "TProxyStopService...")
+            TProxyStopService()
+        } catch (e: Exception) {
+            Log.e(AppConfig.TAG, "Failed to stop hev-socks5-tunnel", e)
+        }
+    }
+}

V2rayNG/app/src/main/java/com/v2ray/ang/service/Tun2SocksControl.kt
@@ -1,0 +1,19 @@
+package com.v2ray.ang.service
+
+/**
+ * Interface that defines the control operations for tun2socks implementations.
+ * 
+ * This interface is implemented by different tunnel solutions like:
+ */
+interface Tun2SocksControl {
+    /**
+     * Starts the tun2socks process with the appropriate parameters.
+     * This initializes the VPN tunnel and connects it to the SOCKS proxy.
+     */
+    fun startTun2Socks()
+    
+    /**
+     * Stops the tun2socks process and cleans up resources.
+     */
+    fun stopTun2Socks()
+}

AnGgIt886/Neko-ray

V2rayNG/app/src/main/java/com/neko/v2ray/service/TProxyService.kt
@@ -1,0 +1,96 @@
+package com.neko.v2ray.service
+
+import android.content.Context
+import android.os.ParcelFileDescriptor
+import android.util.Log
+import com.neko.v2ray.AppConfig
+import com.neko.v2ray.AppConfig.VPN_MTU
+import com.neko.v2ray.handler.MmkvManager
+import com.neko.v2ray.handler.SettingsManager
+import java.io.File
+
+/**
+ * Manages the tun2socks process that handles VPN traffic
+ */
+class TProxyService(
+    private val context: Context,
+    private val vpnInterface: ParcelFileDescriptor,
+    private val isRunningProvider: () -> Boolean,
+    private val restartCallback: () -> Unit
+) : Tun2SocksControl {
+    companion object {
+        @JvmStatic
+        @Suppress("FunctionName")
+        private external fun TProxyStartService(configPath: String, fd: Int)
+        @JvmStatic
+        @Suppress("FunctionName")
+        private external fun TProxyStopService()
+        @JvmStatic
+        @Suppress("FunctionName")
+        private external fun TProxyGetStats(): LongArray?
+
+        init {
+            System.loadLibrary("hev-socks5-tunnel")
+        }
+    }
+
+    /**
+     * Starts the tun2socks process with the appropriate parameters.
+     */
+    override fun startTun2Socks() {
+        Log.i(AppConfig.TAG, "Starting HevSocks5Tunnel via JNI")
+
+        val configContent = buildConfig()
+        val configFile = File(context.filesDir, "hev-socks5-tunnel.yaml").apply {
+            writeText(configContent)
+        }
+        Log.i(AppConfig.TAG, "Config file created: ${configFile.absolutePath}")
+        Log.d(AppConfig.TAG, "Config content:\n$configContent")
+
+        try {
+            Log.i(AppConfig.TAG, "TProxyStartService...")
+            TProxyStartService(configFile.absolutePath, vpnInterface.fd)
+        } catch (e: Exception) {
+            Log.e(AppConfig.TAG, "HevSocks5Tunnel exception: ${e.message}")
+        }
+    }
+
+    private fun buildConfig(): String {
+        val socksPort = SettingsManager.getSocksPort()
+        val vpnConfig = SettingsManager.getCurrentVpnInterfaceAddressConfig()
+        return buildString {
+            appendLine("tunnel:")
+            appendLine("  mtu: $VPN_MTU")
+            appendLine("  ipv4: ${vpnConfig.ipv4Client}")
+
+            if (MmkvManager.decodeSettingsBool(AppConfig.PREF_PREFER_IPV6) == true) {
+                appendLine("  ipv6: '${vpnConfig.ipv6Client}'")
+            }
+
+            appendLine("socks5:")
+            appendLine("  port: ${socksPort}")
+            appendLine("  address: ${AppConfig.LOOPBACK}")
+            appendLine("  udp: 'udp'")
+
+            MmkvManager.decodeSettingsString(AppConfig.PREF_LOGLEVEL)?.let { logPref ->
+                if (logPref != "none") {
+                    val logLevel = if (logPref == "warning") "warn" else logPref
+                    appendLine("misc:")
+                    appendLine("  log-level: $logLevel")
+                }
+            }
+        }
+    }
+
+    /**
+     * Stops the tun2socks process
+     */
+    override fun stopTun2Socks() {
+        try {
+            Log.i(AppConfig.TAG, "TProxyStopService...")
+            TProxyStopService()
+        } catch (e: Exception) {
+            Log.e(AppConfig.TAG, "Failed to stop hev-socks5-tunnel", e)
+        }
+    }
+}

V2rayNG/app/src/main/java/com/neko/v2ray/service/Tun2SocksControl.kt
@@ -1,0 +1,19 @@
+package com.neko.v2ray.service
+
+/**
+ * Interface that defines the control operations for tun2socks implementations.
+ * 
+ * This interface is implemented by different tunnel solutions like:
+ */
+interface Tun2SocksControl {
+    /**
+     * Starts the tun2socks process with the appropriate parameters.
+     * This initializes the VPN tunnel and connects it to the SOCKS proxy.
+     */
+    fun startTun2Socks()
+    
+    /**
+     * Stops the tun2socks process and cleans up resources.
+     */
+    fun stopTun2Socks()
+}